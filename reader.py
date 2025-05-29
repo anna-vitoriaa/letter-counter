80f907000f88735fad15e55f3b066cc0eb1b90e0
Anna Vitoria <vitoria.963369@gmail.com>
2025-05-29 11:24:43 -0300

    Implementando leitura de arquivo

--- reader.py
+++ reader.py
@@ -1,4 +1,6 @@
-texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+# abrindo o arquivo com o texto
+with open('arquivo.txt', 'r', encoding="utf-8") as arquivo:
+    texto = arquivo.read()
 
 alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -7,9 +9,8 @@ dict = {letra: 0 for letra in alph}
 
 # compara as letras do alfabeto com as do texto e salva no dicionário somando a quantidade de cada uma
 for letra in texto.lower():
-    for let in alph:
-        if letra == let:
-            dict[let] = dict[let]+1
+    if letra in alph:
+        dict[letra] = dict[letra]+1
 
 # calculando total de caracteres presentes no alfabeto
 total = 0
