9a3059bf1a8c5b241600979aa5018fda9df5c502
Anna Vitoria <vitoria.963369@gmail.com>
2025-05-29 11:08:05 -0300

    Adicionando exemplos de saída

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
-texto = "Texto teste"
+texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # dicionário com as letras do alfbeto como chave e 0 como valor
@@ -16,7 +17,18 @@ for l in texto.lower():
     if l in alph:
         total+=1
 
-# calculando a porcentagem de cada letra e salvando em um dicionário]
+# calculando a porcentagem de cada letra e salvando em um dicionário
 porc = {letra: 0 for letra in alph}
 for letra, quant in dict.items():
     porc[letra] = quant*100/total
+
+# Saída
+print("QUANTIDADE DE CADA LETRA")
+print('='*50)
+for letra, quant in dict.items():
+    print(f"{letra.upper()}: {quant}")
+print('='*50)
+
+print("PORCENTAGEM DE CADA LETRA")
+for letra, quant in porc.items():
+    print(f"{letra.upper()}: {quant:.1f}%")
